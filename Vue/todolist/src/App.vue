420f28a253809f58bc23f71ec887e3cb0c912b37
amberpark <amberso1996@gmail.com>
2021-07-12 00:45:59 +0900

    weekend

--- Vue/todolist/src/App.vue
+++ Vue/todolist/src/App.vue
@@ -1,27 +1,16 @@
 <template>
   <div id="app">
-    <h1>TodoList</h1>
-    <h2>Todos: {{ todos.length }}</h2>
-    <h2>completed: {{ completedTodosCount.length }}</h2>
-    <h2>Uncompleted: {{ UncompletedTodosCount.length }}</h2>
+   <TodoApp />
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
-import TodoForm from './components/TodoForm.vue'
-import TodoList from './components/TodoList.vue'
+import TodoApp from './components/TodoApp.vue'
+
 export default {
   name: 'App',
   components: {
-    TodoForm, TodoList,
-  },
-  computed: {
-    ...mapGetters([
-      'completedTodosCount',
-      'uncompletedTodosCount',
-    ]),
-    ...mapState(['todos'])
+    TodoApp,
   }
 }
 </script>
